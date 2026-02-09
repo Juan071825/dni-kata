50f43703d1837dc59b36b0a04ba770118fd33c8a
juan071825 <juanmateoalvarez@gmail.com>
2026-02-09 13:22:45 +0100

    test(test_tabla_equivalencias): :test_tube: Añadir casos test.

---
+++ test/test_tabla_equivalencias.py
@@ -0,0 +1,55 @@
+import pytest
+from tablaEquivalencias import TablaEquivalencias
+
+@pytest.fixture(name="tabla")
+def tablaEquivalencias():
+    return TablaEquivalencias()
+
+def test_getTabla(tabla):
+
+    assert tabla.getTabla() == {
+            "T": 0,
+            "R": 1,
+            "W": 2,
+            "A": 3,
+            "G": 4,
+            "M": 5,
+            "Y": 6,
+            "F": 7,
+            "P": 8,
+            "D": 9,
+            "X": 10,
+            "B": 11,
+            "N": 12,
+            "J": 13,
+            "Z": 14,
+            "S": 15,
+            "Q": 16,
+            "V": 17,
+            "H": 18,
+            "L": 19,
+            "C": 20,
+            "K": 21,
+            "E": 22,
+        }
+
+def test_getLetra(tabla):
+    assert tabla.getLetra(4) == "G"
+    assert tabla.getLetra(12) == "N"
+    assert tabla.getLetra(32) == "Posicion letra fuera de rango"
+
+def test_getModulo(tabla):
+    assert tabla.getModulo() == 23
+
+def test_isLetraValida(tabla):
+    assert tabla.isLetraValida("T")
+    assert not tabla.isLetraValida("I")
+
+@pytest.mark.parametrize("dni", CASOS_TEST_CORRECTOS)
+def test_calcularLetra_correcta(tabla, "dni"):
+    numero_dni = dni[:-1]
+    letra_dni = dni[-1]
+
+    assert tabla.calcularLetra(numero_dni) == letra_dni
+
+
