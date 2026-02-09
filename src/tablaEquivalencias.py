75dd4ff5bf206c4c1dc6a2f8a596d31a8b145854
juan071825 <juanmateoalvarez@gmail.com>
2026-02-09 13:19:10 +0100

    feat(tablaEquivalencias): :sparkles: Añadir calcularLetra.

--- src/tablaEquivalencias.py
+++ src/tablaEquivalencias.py
@@ -1,38 +1,38 @@
 class TablaEquivalencias():
 
     def __init__(self):
-        self.tabla = {
-            "T": 0,
-            "R": 1,
-            "W": 2,
-            "A": 3,
-            "G": 4,
-            "M": 5,
-            "Y": 6,
-            "F": 7,
-            "P": 8,
-            "D": 9,
-            "X": 10,
-            "B": 11,
-            "N": 12,
-            "J": 13,
-            "Z": 14,
-            "S": 15,
-            "Q": 16,
-            "V": 17,
-            "H": 18,
-            "L": 19,
-            "C": 20,
-            "K": 21,
-            "E": 22,
-        }
+        self.tabla = (
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+            19,
+            20,
+            21,
+            22,
+        )
 
     def getTabla(self):
         return self.tabla
     
-    def getLetra(self, clave):
+    def getLetra(self, letra):
         try:
-            return self.tabla[clave]
+            return self.tabla[letra]
         except KeyError:
             return "La letra no es válida."
     
@@ -42,13 +42,11 @@ class TablaEquivalencias():
     def isLetraValida(self, letra):
         letra in self.tabla
 
+    def calcularLetra(self, dni):
+        numero_dni = dni[:-1]
+        posicion = int(numero_dni) % self.getModulo()
+        return self.getLetra(posicion)
+
     
     def __repr__(self):
         return str(self.tabla)
-    
-
-    
-
-if __name__ == '__main__':
-    tabla = TablaEquivalencias()
-    print(tabla)
