eb665bd21bdb485c2470dbe2faa87103da76d953
juan071825 <juanmateoalvarez@gmail.com>
2026-02-21 11:01:01 +0100

    test(test_tabla_Equivalencias): :test_tube: Añadir nuevos casos test.

--- test/test_tabla_Equivalencias.py
+++ test/test_tabla_Equivalencias.py
@@ -1,9 +1,12 @@
 import pytest
+from src.tablaEquivalencias import TablaEquivalencias
+from test.test_dni_correctos import TEST_DNI_VALIDOS
+from test.test_dni_incorrectos import TEST_DNI_LETRA_ERRONEA
 
 
 @pytest.fixture(name="tabla")
 def tablaEquivalencias():
-    return tablaEquivalencias()
+    return TablaEquivalencias()
 
 
 def test_getTabla(tabla):
@@ -33,3 +36,16 @@ def test_getTabla(tabla):
         "K",
         "E",
     ]
+
+def test_getLetra(tabla):
+    assert tabla.getLetra(4) == "G"
+    assert tabla.getLetra(12) == "N"
+    assert tabla.getLetra(32) == "La letra no es válida."
+
+def test_getModulo(tabla):
+    assert tabla.getModulo() == 23
+
+def test_isLetraValida(tabla):
+    assert tabla.isLetraValida("T")
+    assert not tabla.isLetraValida("I")
+
